9fced65b90516a8daff80fb879482cf7ac654cf8
Deepak <deepak91g@gmail.com>
2017-09-11 16:24:49 +0100

    Learning with tensor flow

---
+++ tensorFlowLearning.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+import gc
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
