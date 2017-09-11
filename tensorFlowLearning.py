2680eb2759d2c986e7a1922cc7a2b4abc554662b
Deepak <deepak91g@gmail.com>
2017-09-11 16:25:21 +0100

    test and train

--- tensorFlowLearning.py
+++ tensorFlowLearning.py
@@ -5,3 +5,5 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
+train = pd.read_csv(input_path + 'train.csv', nrows=50000000 if is_on_kaggle else 200000)
+test = pd.read_csv(input_path + 'test.csv', nrows=None if is_on_kaggle else 1000000)
