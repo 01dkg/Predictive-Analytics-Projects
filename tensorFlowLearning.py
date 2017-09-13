30909a916ef5a2b300261607cf58781b7162bde4
Deepak <deepak91g@gmail.com>
2017-09-13 16:26:25 +0100

    # convert the time information

--- tensorFlowLearning.py
+++ tensorFlowLearning.py
@@ -17,3 +17,16 @@ train.drop(['attributed_time'], axis=1, inplace=True)
 test_prediction = pd.DataFrame()
 test_prediction['click_id'] = test['click_id']
 test.drop('click_id', axis=1, inplace=True)
+
+def timeEncoding(df):
+    df['click_time'] = pd.to_datetime(df['click_time'])
+
+    df['hour'] = df['click_time'].dt.hour
+    df['weekday'] = df['click_time'].dt.weekday
+
+    df.drop('click_time', axis=1, inplace=True)
+    return df
+
+
+train = timeEncoding(train)
+test = timeEncoding(test)
