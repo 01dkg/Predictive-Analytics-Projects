82c17bddf506d210d0fb8edb6c81378fa3af4058
Deepak <deepak91g@gmail.com>
2017-09-25 16:45:10 +0100

    update

--- tensorFlowLearning.py
+++ tensorFlowLearning.py
@@ -43,15 +43,15 @@ train_dm = scaler.transform(train)
 del train
 gc.collect()
 
-train_dm_non_anomalystic = train_dm[np.where(response == 0)[0]]
-train_dm_anomalystic = train_dm[np.where(response == 1)[0]]
-train_dm_non_anomalystic, validation_dm_non_anomalystic = train_test_split(train_dm_non_anomalystic, test_size=0.2)
+train_dm_non_anomaly = train_dm[np.where(response == 0)[0]]
+train_dm_anomaly = train_dm[np.where(response == 1)[0]]
+train_dm_non_anomaly, validation_dm_non_anomaly = train_test_split(train_dm_non_anomaly, test_size=0.2)
 
 niter =2200 if is_on_kaggle else 1500
-batch_size = 500000 if np.shape(train_dm_non_anomalystic)[0] > 1000000 else int(np.shape(train_dm_non_anomalystic)[0] / 5)
+batch_size = 500000 if np.shape(train_dm_non_anomaly)[0] > 1000000 else int(np.shape(train_dm_non_anomaly)[0] / 5)
 learning_rate = 0.004
 
-nOf_features = len(train_dm_non_anomalystic[0])
+nOf_features = len(train_dm_non_anomaly[0])
 nOf_neurons_first_layer = 6 
 nOf_neurons_second_layer = 3
 We1 = tf.Variable(tf.random_normal([nOf_features, nOf_neurons_first_layer], dtype=tf.float32))
@@ -87,14 +87,14 @@ loss_valid = []
 loss_anomaly = []
 for i in range(niter):
     if batch_size > 0:
-    	for j in range(np.shape(train_dm_non_anomalystic)[0] // batch_size):
-            batch_data = get_batch(train_dm_non_anomalystic, j, batch_size)
+    	for j in range(np.shape(train_dm_non_anomaly)[0] // batch_size):
+            batch_data = get_batch(train_dm_non_anomaly, j, batch_size)
             sess.run(train_step, feed_dict={X: batch_data})
     else:
-        sess.run(train_step, feed_dict={X: train_dm_non_anomalystic})
-    lt = sess.run(loss, feed_dict={X: train_dm_non_anomalystic})
-    lv = sess.run(loss, feed_dict={X: validation_dm_non_anomalystic})
-    la = sess.run(loss, feed_dict={X: train_dm_anomalystic})
+        sess.run(train_step, feed_dict={X: train_dm_non_anomaly})
+    lt = sess.run(loss, feed_dict={X: train_dm_non_anomaly})
+    lv = sess.run(loss, feed_dict={X: validation_dm_non_anomaly})
+    la = sess.run(loss, feed_dict={X: train_dm_anomaly})
     loss_train.append(lt)
     loss_valid.append(lv)
     loss_anomaly.append(la)
@@ -113,13 +113,13 @@ plt.xlabel('niter')
 plt.legend(['train (w/o anomaly)', 'valid (w/o anomaly)', 'anomalistic data'], loc='upper right')
 plt.gcf().savefig('learning_curves.png') if is_on_kaggle else plt.show()
 np.random.seed(1)
-sampled_train_dm_non_anomalystic = train_dm_non_anomalystic[np.random.choice(len(train_dm_non_anomalystic), 5 * np.shape(train_dm_anomalystic)[0], replace=False)]
-non_anomalystic_reconstructed = sess.run(decoded, feed_dict={X: sampled_train_dm_non_anomalystic})
-anomalystic_reconstructed = sess.run(decoded, feed_dict={X: train_dm_anomalystic})
+sampled_train_dm_non_anomaly = train_dm_non_anomaly[np.random.choice(len(train_dm_non_anomaly), 5 * np.shape(train_dm_anomaly)[0], replace=False)]
+non_anomaly_reconstructed = sess.run(decoded, feed_dict={X: sampled_train_dm_non_anomaly})
+anomaly_reconstructed = sess.run(decoded, feed_dict={X: train_dm_anomaly})
 
-non_anomalystic_reconstructed_loss = np.sqrt(np.mean(np.square(sampled_train_dm_non_anomalystic - non_anomalystic_reconstructed), axis=1))
-anomalystic_reconstructed_loss = np.sqrt(np.mean(np.square(train_dm_anomalystic - anomalystic_reconstructed), axis=1))
-del train_dm_non_anomalystic, train_dm_anomalystic
+non_anomaly_reconstructed_loss = np.sqrt(np.mean(np.square(sampled_train_dm_non_anomaly - non_anomaly_reconstructed), axis=1))
+anomaly_reconstructed_loss = np.sqrt(np.mean(np.square(train_dm_anomaly - anomaly_reconstructed), axis=1))
+del train_dm_non_anomaly, train_dm_anomaly
 gc.collect()
 
 test_dm = scaler.transform(test)
